a3ac7f48dedf61d9bc1910d5330db03b41c41786
Pialex99 <alexandre.pinazza@epfl.ch>
2022-12-23 17:31:09 +0100

    Remove calls to tick to an exhale and an inhale for error reporting

--- prusti-viper/src/encoder/builtin_encoder.rs
+++ prusti-viper/src/encoder/builtin_encoder.rs
@@ -22,7 +22,6 @@ pub enum BuiltinMethodKind {
     HavocBV(vir::BitVector),
     HavocRef,
     BumpMemVersion,
-    Tick,
 }
 
 #[derive(Clone, Debug, Hash, Eq, PartialEq)]
@@ -79,7 +78,6 @@ impl<'p, 'v: 'p, 'tcx: 'v> BuiltinEncoder<'p, 'v, 'tcx> {
             BuiltinMethodKind::HavocF64 => "builtin$havoc_f64".to_string(),
             BuiltinMethodKind::HavocRef => "builtin$havoc_ref".to_string(),
             BuiltinMethodKind::BumpMemVersion => versioning::bump_mem_version_name().to_string(),
-            BuiltinMethodKind::Tick => resources::tick_name().to_string(),
         }
     }
 
@@ -93,9 +91,6 @@ impl<'p, 'v: 'p, 'tcx: 'v> BuiltinEncoder<'p, 'v, 'tcx> {
             BuiltinMethodKind::HavocRef => vir::Type::typed_ref(""),
             BuiltinMethodKind::BumpMemVersion => {
                 return versioning::bump_mem_version_definition();
-            }
-            BuiltinMethodKind::Tick =>{
-                return resources::tick_definition();
             }
         };
         vir::BodylessMethod {
